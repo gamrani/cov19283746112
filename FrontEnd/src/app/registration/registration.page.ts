f9dc0ae98e399f457e144c5a8c920494bfd1a3bd
EL GAMRANI <youssef.elgamrani@cgi.com>
2018-10-30 13:33:00 +0100

    customize form

--- FrontEnd/src/app/registration/registration.page.ts
+++ FrontEnd/src/app/registration/registration.page.ts
@@ -10,16 +10,28 @@ export class RegistrationPage implements OnInit {
   
   hideFirstCard = false;
   hideSecondCard = true;
+  hideThirdCard = true;
+  step1 = true;
+  step2 = true;
+  step3 = true;
 
   user = new User();
 
   submit(){
-    console.log(this.user);
+    this.step3=false;
   }
   constructor() { }
-  continuer(){
+  card1done(){
     this.hideFirstCard=true;
     this.hideSecondCard=false;
+    this.hideThirdCard=true;
+    this.step1 = false;
+ }
+ card2done(){
+  this.hideFirstCard=true;
+  this.hideSecondCard=true;
+  this.hideThirdCard=false;
+  this.step2 = false;
  }
   ngOnInit() {
     this.hideFirstCard = false;
